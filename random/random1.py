5f1f88b67511726fe6f5d41c95d137044b3f8610
Boris <borisedison@gmail.com>
2022-01-20 18:48:39 +0530

    added random folder

---
+++ random/random1.py
@@ -0,0 +1,14 @@
+import random
+import my_module
+
+print(my_module.pi)
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+#0.000000000 - 0.9999999999....
+random_float = random.random()
+print(random_float)
+
+randomFloat = random.random()*5      #random number between 0 and 5
+print(randomFloat)
